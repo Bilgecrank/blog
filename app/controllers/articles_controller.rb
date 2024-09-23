55ddf951d0428e205e309c3d7467dea4ed5b2d19
bilge <8432868+Bilgecrank@users.noreply.github.com>
2024-09-23 15:28:05 -0400

    Set up creation logic for articles
    Still working on the tutorial section for Rails.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,30 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    if params[:id] =~ /\A\d+\z/ ? true : false
+      @article = Article.find(params[:id])
+    else
+      redirect_to root_path
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
