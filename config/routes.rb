9b653c0067e07be425b74b1d1764a823da7f58de
bilge <8432868+Bilgecrank@users.noreply.github.com>
2024-09-24 12:55:23 -0400

    Added in a comments functionality.
    Still going through the tutorial.

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "articles#index"
-
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
